fbd5a9f124fd28df46007d9e72d2dbab3935ad05
Tsurumaki Ayaka <ayaka.tsurumaki@sitateru.com>
2019-04-25 12:04:01 +0900

    add list pullrequest branches

--- main.go
+++ main.go
@@ -11,33 +11,39 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var GITHUB_TOKEN = ""
-var GITHUB_OWNER = ""
+var GITHUB_TOKEN = os.Getenv("GITHUB_TOKEN")
+var GITHUB_OWNER = os.Getenv("GITHUB_OWNER")
 
-func main() {
-	GITHUB_TOKEN = os.Getenv("GITHUB_TOKEN")
-	GITHUB_OWNER = os.Getenv("GITHUB_OWNER")
+var app = cli.NewApp()
 
-	app := cli.NewApp()
-	app.Action = listBranches
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
+func commands() {
+	app.Commands = []cli.Command{
+		{
+			Name:   "branch",
+			Action: listBranches,
+		},
+		{
+			Name:   "pbranch",
+			Action: listPullRequestBranches,
+		},
 	}
 }
 
-func listBranches(c *cli.Context) error {
+func newClient() *github.Client {
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
 		&oauth2.Token{AccessToken: GITHUB_TOKEN},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
-	client := github.NewClient(tc)
+	return github.NewClient(tc)
+}
+
+func listBranches(c *cli.Context) error {
+	client := newClient()
 
 	opt := &github.ListOptions{}
-	branches, _, err := client.Repositories.ListBranches(ctx, GITHUB_OWNER, c.Args()[0], opt)
+	branches, _, err := client.Repositories.ListBranches(context.Background(), GITHUB_OWNER, c.Args()[0], opt)
 	if err != nil {
 		return err
 	}
@@ -47,4 +53,29 @@ func listBranches(c *cli.Context) error {
 	}
 
 	return nil
+}
+
+func listPullRequestBranches(c *cli.Context) error {
+	client := newClient()
+
+	opt := &github.PullRequestListOptions{}
+	pullrequests, _, err := client.PullRequests.List(context.Background(), GITHUB_OWNER, c.Args()[0], opt)
+	if err != nil {
+		return err
+	}
+
+	for _, p := range pullrequests {
+		fmt.Println(p.GetHead().GetRef())
+	}
+
+	return nil
+}
+
+func main() {
+	commands()
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
